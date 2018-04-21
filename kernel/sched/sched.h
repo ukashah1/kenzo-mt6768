4612520fe31a1947ce7ebde87c7012c5843c8963
Puja Gupta <pujag@codeaurora.org>
2018-04-20 17:24:43 -0700

    sched: Fix incorrect usage of SCHED_CPUFREQ_INTERCLUSTER_MIG flag
    Mark the source/destination CPUs correctly for inter cluster
    migration.
    
    Change-Id: I771b9357d20cb0270465abd594fb94bb3669c936
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: Puja Gupta <pujag@codeaurora.org>
    Signed-off-by: VoidDev-0 <dev.avax@protonmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -962,6 +962,7 @@ struct rq {
 	u8 curr_table;
 	int prev_top;
 	int curr_top;
+	bool notif_pending;
 #endif /* CONFIG_SCHED_WALT */
 
 #ifdef CONFIG_IRQ_TIME_ACCOUNTING
@@ -2580,6 +2581,25 @@ DECLARE_PER_CPU(struct update_util_data *, cpufreq_update_util_data);
 static inline void cpufreq_update_util(struct rq *rq, unsigned int flags)
 {
 	struct update_util_data *data;
+
+#ifdef CONFIG_SCHED_WALT
+	unsigned int exception_flags = SCHED_CPUFREQ_INTERCLUSTER_MIG |
+				SCHED_CPUFREQ_PL | SCHED_CPUFREQ_EARLY_DET |
+				SCHED_CPUFREQ_FORCE_UPDATE;
+
+	/*
+	 * Skip if we've already reported, but not if this is an inter-cluster
+	 * migration. Also only allow WALT update sites.
+	 */
+	if (!(flags & SCHED_CPUFREQ_WALT))
+		return;
+	if (!sched_disable_window_stats &&
+		(rq->load_reported_window == rq->window_start) &&
+		!(flags & exception_flags))
+		return;
+	if (!(flags & exception_flags))
+		rq->load_reported_window = rq->window_start;
+#endif
 
 	data = rcu_dereference_sched(*per_cpu_ptr(&cpufreq_update_util_data,
 						  cpu_of(rq)));
