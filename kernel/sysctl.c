3396c84a8e6ae0f079055d027bf20a3c0bd06cc2
freak07 <michalechner92@googlemail.com>
2019-05-30 21:51:43 +0200

    sysctl: change max swappiness
    This is done for example on HTC U12+, which also features 6GB of RAM along with zram
    
    (cherry picked from commit 38b176b31b7f370d4f79a6bf3b90d794ff824f37)
    (cherry picked from commit 72b62efb9a134a6b88718ea133e7a843cc62539d)
    (cherry picked from commit bb23dc85117e3e187e8a79d08cb24215b5a60deb)
    (cherry picked from commit 0a43761c81aeb1e3e6d4575d63b8d578815109c2)
    (cherry picked from commit 3a88785e53790a7603068bfed8b8c0db4bd1f200)
    (cherry picked from commit ba5013462e90d15ef120f314f0f15ba8a60ec664)
    (cherry picked from commit 56b108414ccec1ef4ddbaaa0b2fd1b1d91f5d287)

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -133,6 +133,9 @@ static int one_hundred = 100;
 static int two_hundred = 200;
 #endif
 static int one_thousand = 1000;
+
+static int max_swappiness = 200;
+
 #ifdef CONFIG_PRINTK
 static int ten_thousand = 10000;
 #endif
@@ -1492,7 +1495,7 @@ static struct ctl_table vm_table[] = {
 		.proc_handler	= proc_dointvec_minmax,
 		.extra1		= &zero,
 #ifndef CONFIG_MTK_GMO_RAM_OPTIMIZE
-		.extra2		= &one_hundred,
+		.extra2		= &max_swappiness,
 #else
 		.extra2		= &two_hundred,
 #endif
